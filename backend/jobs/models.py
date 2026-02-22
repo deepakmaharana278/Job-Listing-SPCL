8d18245a23bbd611d018ea74e9ebbdf1173af667
Jitendra Maharana <deepakmaharana3500@gmail.com>
2026-02-22 21:02:55 +0530

    Authentication api added

--- backend/jobs/models.py
+++ backend/jobs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=100,unique=True)
+    password = models.CharField(max_length=50)
+    career_level = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name}"
--- backend/jobs/views.py
+++ backend/jobs/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.response import  Response
+from rest_framework.decorators import  api_view
+from django.contrib.auth.hashers import make_password,check_password
+from .models import *
 
-# Create your views here.
+@api_view(['POST'])
+def register(req):
+    name = req.data.get('name')
+    email = req.data.get('email')
+    password = req.data.get('password')
+    career_level = req.data.get('career_level')
+
+    if User.objects.filter(email=email).exists():
+        return Response({'message':'Email already exists'},status=400)
+    
+    user = User.objects.create(name=name,email=email,password=make_password(password),career_level=career_level)
+
+    return Response({'message':f'{name} registered successfully'},status=201)
+
+@api_view(['POST'])
+def login(req):
+    email = req.data.get('email')
+    password = req.data.get('password')
+
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response({'error': 'Invalid email or password'}, status=400)
+
+    if not check_password(password, user.password):
+        return Response({'error': 'Invalid password'}, status=400)
+
+    return Response({'message': 'Login successful'},status=200)
